143e599a7b0d3195cadd2c1e8234e1b84f77af73
mesheikbrown <Mesheik@github.com>
2026-04-14 16:46:29 -0400

    Add SpringBoot and Test Units

---
+++ src/main/java/com/team2/mentalhealthtracker/MentalHealthTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.team2.mentalhealthtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentalHealthTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentalHealthTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team2/mentalhealthtracker/MentalHealthTrackerApplicationTests.java
@@ -0,0 +1,15 @@
+package test.java.com.team2.mentalhealthtracker;
+
+import com.team2.mentalhealthtracker.SpringBootTest;
+import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentalHealthTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
