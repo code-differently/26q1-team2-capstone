8cd7ab760659b828300dfa2cc8a28bf21d606b97
mesheikbrown <Mesheik@github.com>
2026-04-15 16:01:18 -0400

    Updated more classes

--- src/main/java/com/team2/mentalhealthtracker/controller/MoodEntryController.java
+++ src/main/java/com/team2/mentalhealthtracker/controller/MoodEntryController.java
@@ -1,4 +1,29 @@
 package com.team2.mentalhealthtracker.controller;
 
+import com.team2.mentalhealthtracker.model.MoodEntry;
+import com.team2.mentalhealthtracker.service.MoodEntryService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/mood-entries")
+@CrossOrigin(origins = "http://localhost:3000")
 public class MoodEntryController {
+
+    private final MoodEntryService moodEntryService;
+
+    public MoodEntryController(MoodEntryService moodEntryService) {
+        this.moodEntryService = moodEntryService;
+    }
+
+    @PostMapping
+    public MoodEntry createMoodEntry(@RequestBody MoodEntry moodEntry) {
+        return moodEntryService.saveMoodEntry(moodEntry);
+    }
+
+    @GetMapping
+    public List<MoodEntry> getAllMoodEntries() {
+        return moodEntryService.getAllMoodEntries();
+    }
 }
--- src/main/java/com/team2/mentalhealthtracker/service/MoodEntryService.java
+++ src/main/java/com/team2/mentalhealthtracker/service/MoodEntryService.java
@@ -1,4 +1,25 @@
 package com.team2.mentalhealthtracker.service;
 
+import com.team2.mentalhealthtracker.model.MoodEntry;
+import com.team2.mentalhealthtracker.repository.MoodEntryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class MoodEntryService {
+
+    private final MoodEntryRepository moodEntryRepository;
+
+    public MoodEntryService(MoodEntryRepository moodEntryRepository) {
+        this.moodEntryRepository = moodEntryRepository;
+    }
+
+    public MoodEntry saveMoodEntry(MoodEntry moodEntry) {
+        return moodEntryRepository.save(moodEntry);
+    }
+
+    public List<MoodEntry> getAllMoodEntries() {
+        return moodEntryRepository.findAll();
+    }
 }
