2df4f85e7702306af842f164dd5759db06f440bc
glenntyson <gatyson261@gmail.com>
2026-04-15 09:33:25 -0400

    Encrptyed the DB password

--- src/test/java/com/team2/mentalhealthtracker/MentalHealthTrackerApplicationTests.java
+++ src/test/java/com/team2/mentalhealthtracker/MentalHealthTrackerApplicationTests.java
@@ -1,7 +1,5 @@
-package test.java.com.team2.mentalhealthtracker;
+package com.team2.mentalhealthtracker;
 
-import com.team2.mentalhealthtracker.SpringBootTest;
-import org.junit.Test;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
