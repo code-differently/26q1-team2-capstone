e5e486dfb8adc3d944e5699710bc305e036419a1
mesheikbrown <Mesheik@github.com>
2026-04-16 14:47:31 -0400

    Trigger Controller

--- src/main/java/com/team2/mentalhealthtracker/controller/TriggerEntryController.java
+++ src/main/java/com/team2/mentalhealthtracker/controller/TriggerEntryController.java
@@ -1,4 +1,29 @@
 package com.team2.mentalhealthtracker.controller;
 
+import com.team2.mentalhealthtracker.model.TriggerEntry;
+import com.team2.mentalhealthtracker.service.TriggerEntryService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/triggers")
+@CrossOrigin(origins = "http://localhost:3000")
 public class TriggerEntryController {
+
+    private final TriggerEntryService triggerEntryService;
+
+    public TriggerEntryController(TriggerEntryService triggerEntryService) {
+        this.triggerEntryService = triggerEntryService;
+    }
+
+    @PostMapping
+    public TriggerEntry createTriggerEntry(@RequestBody TriggerEntry triggerEntry) {
+        return triggerEntryService.saveTriggerEntry(triggerEntry);
+    }
+
+    @GetMapping
+    public List<TriggerEntry> getAllTriggerEntries() {
+        return triggerEntryService.getAllTriggerEntries();
+    }
 }
